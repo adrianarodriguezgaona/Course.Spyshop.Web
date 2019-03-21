f9bfc390ad6bc58602d8f160388ec88c6e7104bb
adrianitargaona <adrianitargaona@gmail.com>
2019-03-21 16:07:39 +0100

    Rest van de les

---
+++ Course.Spyshop.Web/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Course.Spyshop.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SearchByName(string productName)
+        {
+            ViewData["SearchMethod"] = "Opgezocht via tekst";
+            ViewData["SearchSubject"] = productName;
+
+            return View("SearchResults");
+        }
+
+        public IActionResult SearchById(string id)
+        {
+            ViewData["SearchMethod"] = "Opgezocht via id nummer";
+            ViewData["SearchSubject"] = id;
+
+            return View("SearchResults");
+        }
+    }
+}
--- Course.Spyshop.Web/Startup.cs
+++ Course.Spyshop.Web/Startup.cs
@@ -15,6 +15,8 @@ namespace Course.Spyshop.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,11 +26,39 @@ namespace Course.Spyshop.Web
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            else
             {
-                await context.Response.WriteAsync("Hello World!");
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "searchById",
+                    template: "Search/{id:long}",
+                    defaults: new {controller = "Home",action = "SearchById"}
+                    
+                    );
+                routes.MapRoute(
+                   name: "default",
+                   template: "{controller = Home}/{action=Index}/{id?}"
+                   //defaults: new { controller = "Home", action = "Index" }
+                   );
             });
+
+            app.UseMvcWithDefaultRoute();
+
+            //    app.Use(async (context, next) => {
+            //        if (context.Request.Path == "/spyshop")
+            //            await context.Response.WriteAsync("Spy shop");
+            //        else
+            //            await next.Invoke();
+
+            //});
+
+            //    app.Run(async (context) =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
         }
     }
 }
